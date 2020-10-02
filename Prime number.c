ca62ac35842bf8735ce60bd9432749304bb28a8d
HarnishSavsani <68416821+HarnishSavsani@users.noreply.github.com>
2020-10-02 17:54:13 +0530

    successfully updated
    added colours

--- Prime number.c
+++ Prime number.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int Prime(int,int);
 
@@ -6,16 +7,22 @@ int main()
 {
 
     int num,prime;
-
+    system("color fd");
     printf("Enter any number: ");
     scanf("%d",&num);
 
     prime = Prime(num,num/2);
 
     if(prime==1)
+    {
+        system("color f3");
         printf("%d is a prime number",num);
+    }
     else
+    {
+        system("color f1");
         printf("%d is not a prime number",num);
+    }
 
     return 0;
 }
