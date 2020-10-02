f77affe87456366015aed420d79143d3d1438a86
HarnishSavsani <68416821+HarnishSavsani@users.noreply.github.com>
2020-10-02 17:50:09 +0530

    successfully updated
    added colours in the program

--- Swapping.c
+++ Swapping.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
     int a,b;
+    system("color fd");
     printf("Enter two numbers: \n");
     scanf("%d%d",&a,&b);
-      
+    
     printf("\nBefore swapping: \na=%d and b=%d",a,b);  
  
     a=a+b;
     b=a-b;
     a=a-b; 
-    
+    system("color f1");
     printf("\n\nAfter swapping: \na=%d and b=%d",a,b);
     return 0;
 }
